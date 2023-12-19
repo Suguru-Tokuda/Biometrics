e9744ca9d1bcfe43eeb32bea8ac8ab8259e5c278
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-12-19 17:38:33 -0500

    Bug fix & added a success view.

---
+++ LocalAuthFrameowkrIntegration/Views/AuthenticationSuccessView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationSuccessView.swift
+//  LocalAuthFrameowkrIntegration
+//
+//  Created by Suguru Tokuda on 12/19/23.
+//
+
+import SwiftUI
+
+struct AuthenticationSuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationSuccessView()
+}
