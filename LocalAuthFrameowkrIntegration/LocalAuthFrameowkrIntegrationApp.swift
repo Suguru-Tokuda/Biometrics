fff2adf9f63618e7c6b4271ae0116f8d25a58cd3
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-12-19 09:27:30 -0500

    Initial Commit

---
+++ LocalAuthFrameowkrIntegration/LocalAuthFrameowkrIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAuthFrameowkrIntegrationApp.swift
+//  LocalAuthFrameowkrIntegration
+//
+//  Created by Suguru Tokuda on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalAuthFrameowkrIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
